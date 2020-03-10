df66c692510b536c74d9f457daa3e99688fa6010
deepakb2510 <deepbhan23@gmail.com>
2020-03-10 22:04:47 +0530

    NEw app

--- deepak/app/src/main/java/com/example/myapplication/MainActivity.java
+++ deepak/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
 
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSubtract,btnDivide,btnMultiply,btnEqual,btnClear,btnBracket,btnDot,btnPercent;
     TextView tvInput,tvOutput;
+    ImageButton btnSpeak;
     String process;
     boolean checkBracket=false;
     @Override
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btn8=findViewById(R.id.btn8);
         btn9=findViewById(R.id.btn9);
 
+
         btnAdd=findViewById(R.id.btnAdd);
         btnSubtract=findViewById(R.id.btnSubtract);
         btnMultiply=findViewById(R.id.btnMultiply);
@@ -42,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         btnBracket=findViewById(R.id.btnBracket);
         btnDot=findViewById(R.id.btnDot);
         btnEqual=findViewById(R.id.btnEqual);
+        btnSpeak=findViewById(R.id.btnSpeak);
 
         tvInput=findViewById(R.id.tvInput);
         tvOutput=findViewById(R.id.tvOutput);
@@ -181,6 +192,22 @@ public class MainActivity extends AppCompatActivity {
             }
             }
         });
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 10);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Your Device Dont support Speech",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+        });
         btnEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -201,6 +228,20 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    tvInput.setText(result.get(0));
+                }
+                break;
+        }
     }
+
 }
